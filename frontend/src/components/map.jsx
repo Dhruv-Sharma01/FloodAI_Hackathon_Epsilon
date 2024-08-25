a2557c81c28f79d317aaef8530a4516eb7a6c023
Prajas Kulkarni <zero20206@outlook.com>
2024-08-25 11:19:06 +0530

    added map

--- frontend/src/components/map.jsx
+++ frontend/src/components/map.jsx
@@ -6,7 +6,7 @@ function Map(){
         // return map here
         <div className="map-section">
         <iframe
-            src="./../mumbai_population_map_with_elevation.html"
+            src="./../flood_risk_map_with_clusters.html"
             title="Mumbai Population Map"
             width={"100%"}
             height={"100%"}
