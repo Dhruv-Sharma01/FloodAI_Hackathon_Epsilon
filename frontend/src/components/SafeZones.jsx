014d6239515a2946b9731ae1cef7dd90cf25342f
Prajas Kulkarni <zero20206@outlook.com>
2024-08-25 14:12:27 +0530

    almost done

--- frontend/src/components/SafeZones.jsx
+++ frontend/src/components/SafeZones.jsx
@@ -13,7 +13,29 @@ function SafeZone() {
             </div>
 
             <div className="Emergency">
-                <div className="heading">Emergency Number</div>
+                <div className="heading">Guidelines</div>
+                <p className='guidelines'>
+                <ul>
+        <li>Monitor weather updates and emergency alerts.</li>
+        <li>Pack an emergency kit with essential supplies.</li>
+        <li>Plan and know your evacuation routes.</li>
+        <li>Secure your home and turn off utilities.</li>
+        <li>Evacuate immediately if advised by authorities.</li>
+        <li>Avoid walking or driving through floodwaters.</li>
+        <li>Move to higher ground if trapped.</li>
+        <li>If driving, turn around and find a safe route.</li>
+        <li>Keep your phone charged and stay in touch.</li>
+        <li>Listen to and follow official instructions.</li>
+        <li>Avoid contact with contaminated floodwater.</li>
+        <li>Wear protective clothing and prevent hypothermia.</li>
+        <li>Return home only when it's declared safe.</li>
+        <li>Document property damage for insurance.</li>
+        <li>Clean up safely and dispose of hazardous items.</li>
+        <li>Seek assistance from disaster relief services.</li>
+        <li>Watch for secondary hazards like landslides.</li>
+        <li>Continue monitoring local news for updates.</li>
+      </ul>
+                </p>
             </div>
         </div>
     )
--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,13 +1,17 @@
+import React, { useState } from 'react';
+import DisasterContactModal from './disastermodal';
 import './navbar.css'
 import UserModal from './usermodal'
 
 function Navbar(){
-    
+    const [modalIsOpen, setModalIsOpen] = useState(false);
     return(
         <nav>
             <div className="site-name">Epsilon Solutions</div>
             <div className="btn-section">
-                <button type='button' className='btn'>Call Help</button>
+                <button type='button' className='btn' onClick={() => setModalIsOpen(true)}>Call Help</button>
+                <DisasterContactModal isOpen={modalIsOpen} 
+        onClose={() => setModalIsOpen(false)}  />
                 <UserModal />
             </div>
         </nav>
